4b72de336f558816eac2a623821d2de4f62c9a31
Randy <randygoitia7@gmail.com>
2022-11-09 23:09:38 -0400

    feat: se realizo la conexion con firebas

--- app/src/main/java/com/example/myapplication/ui/catalogo/CatalogoFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/catalogo/CatalogoFragment.kt
@@ -1,15 +1,25 @@
 package com.example.myapplication.ui.catalogo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.Toast
+import com.example.myapplication.CustomAdapter
+import com.example.myapplication.Producto
 import com.example.myapplication.R
+import com.google.firebase.firestore.*
 
 
 class CatalogoFragment : Fragment() {
 
+    private var adapter: CustomAdapter? = null
+    private lateinit var arrayListProducto: ArrayList<Producto>
+    private lateinit var db: FirebaseFirestore
+    private lateinit var itemsListView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +29,40 @@ class CatalogoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        val root: View = inflater.inflate(R.layout.fragment_catalogo , container ,false)
+        itemsListView = root.findViewById(R.id.list_view_items)
+        arrayListProducto= arrayListOf()
 
 
+        adapter = CustomAdapter(activity!!, arrayListProducto)
+        itemsListView.adapter = adapter
+        EventChangeListener()
+        return root
+    }
 
-        return inflater.inflate(R.layout.fragment_catalogo, container, false)
+    private fun EventChangeListener(){
+        db = FirebaseFirestore.getInstance()
+        db.collection("Producto").addSnapshotListener(object : EventListener<QuerySnapshot>
+        {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null){
+                    Log.e("Firestore error",error.message.toString())
+                    return
+                }
+                for (dc : DocumentChange in value?.documentChanges!!){
+                    if (dc.type == DocumentChange.Type.ADDED){
+                        val productFirebase = dc.document.toObject(Producto::class.java)
+                        productFirebase.id_product = dc.document.id
+                        arrayListProducto.add(productFirebase)
+
+                    }
+                }
+                adapter!!.notifyDataSetChanged()
+            }
+        }
+        )
+
     }
 
 }
