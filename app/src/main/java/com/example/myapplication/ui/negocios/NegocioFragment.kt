66e9c28480feae81d523c7e51c2152f723786c28
Randy <randygoitia7@gmail.com>
2022-11-15 23:21:40 -0400

    creando conexion con la coleccion negocios de FireBase

--- app/src/main/java/com/example/myapplication/ui/negocios/NegocioFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/negocios/NegocioFragment.kt
@@ -1,60 +1,56 @@
 package com.example.myapplication.ui.negocios
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.Negocio
 import com.example.myapplication.R
+import com.google.firebase.firestore.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NegocioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NegocioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var negocioArrayList: ArrayList<Negocio>
+    private lateinit var db: FirebaseFirestore
+
+
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_negocio, container, false)
+    ): View {
+
+        val root: View = inflater.inflate(R.layout.fragment_negocio , container ,false)
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NegocioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NegocioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun EventChangeListener(){
+        db = FirebaseFirestore.getInstance()
+        db.collection("negocios").addSnapshotListener(object : EventListener<QuerySnapshot>
+        {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null){
+                    Log.e("Firestore error",error.message.toString())
+                    return
                 }
+                for (dc : DocumentChange in value?.documentChanges!!){
+                    if (dc.type == DocumentChange.Type.ADDED){
+                        negocioArrayList.add(dc.document.toObject(Negocio::class.java))
+
+                    }
+                }
+                
             }
+        }
+        )
+
     }
+
+
 }
