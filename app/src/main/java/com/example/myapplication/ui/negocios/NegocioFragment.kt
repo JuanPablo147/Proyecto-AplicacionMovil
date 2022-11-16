396cff17b6d06ca186a8552f3a426517c46195cd
Randy <randygoitia7@gmail.com>
2022-11-15 23:11:35 -0400

    corrigiendo nombre del fragmento

--- app/src/main/java/com/example/myapplication/ui/negocios/NegocioFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/negocios/NegocioFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CrearNegocioFragment.newInstance] factory method to
+ * Use the [NegocioFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CrearNegocioFragment : Fragment() {
+class NegocioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CrearNegocioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_negocio, container, false)
+        return inflater.inflate(R.layout.fragment_negocio, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CrearNegocioFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CrearNegocioFragment.
+         * @return A new instance of fragment NegocioFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CrearNegocioFragment().apply {
+            NegocioFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
