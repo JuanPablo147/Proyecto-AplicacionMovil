33cd29f0314878ecb8f78be595f022446902868f
Brayan <115055362+brayandep@users.noreply.github.com>
2022-12-08 08:47:13 -0400

    agregando parametros

--- app/src/main/java/com/example/myapplication/Producto.java
+++ app/src/main/java/com/example/myapplication/Producto.java
@@ -6,12 +6,15 @@ public class Producto  {
     private Double Costo;
     private String descripcion;
     private String Nombre_Producto;
-    private String Negocio;
+    private String negocio;
     private String tipo_comida;
     private String ruta_imagen;
     private String estado;
     private String id_product;
-
+    private String id_negocio;
+    private String nombre_negocio;
+    private int telefono;
+    private String id_telefono;
     public Producto() {
 
     }
@@ -23,21 +26,48 @@ public class Producto  {
                 "Costo=" + Costo +
                 ", descripcion='" + descripcion + '\'' +
                 ", Nombre_Producto='" + Nombre_Producto + '\'' +
-                ", Negocio='" + Negocio + '\'' +
+                ", Negocio='" + negocio + '\'' +
                 ", tipo_comida='" + tipo_comida + '\'' +
                 ", ruta_imagen='" + ruta_imagen + '\'' +
                 ", estado='" + estado + '\'' +
+                ", telefono='" + telefono + '\'' +
                 '}';
+    }
+
+    public String getNombre_negocio() {
+        return nombre_negocio;
+    }
+
+    public void setNombre_negocio(String nombre_negocio) {
+        this.nombre_negocio = nombre_negocio;
+    }
+
+    public int getNombre_telefono() {
+        return telefono;
+    }
+
+    public void setNombre_telefono(int telefono) {
+        this.telefono = telefono;
     }
 
     public Producto(Double costo, String descripcion, String nombre_Producto, String negocio, String tipo_comida, String ruta_imagen, String estado) {
         Costo = costo;
         this.descripcion = descripcion;
         Nombre_Producto = nombre_Producto;
-        Negocio = negocio;
+        this.negocio = negocio;
         this.tipo_comida = tipo_comida;
         this.ruta_imagen = ruta_imagen;
         this.estado = estado;
+        //this.telefono = telefono;
+
+    }
+
+    public String getId_negocio() {
+        return id_negocio;
+    }
+
+    public void setId_negocio(String id_negocio) {
+        this.id_negocio = id_negocio;
     }
 
     public String getId_product() {
@@ -73,11 +103,19 @@ public class Producto  {
     }
 
     public String getNegocio() {
-        return Negocio;
+        return negocio;
     }
 
     public void setNegocio(String negocio) {
-        Negocio = negocio;
+        this.negocio = negocio;
+    }
+
+    public int gettelefono() {
+        return telefono;
+    }
+
+    public void settelefono(int telefono) {
+        this.telefono = telefono;
     }
 
     public String getTipo_comida() {
